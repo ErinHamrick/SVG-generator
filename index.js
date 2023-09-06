1e01c8c25d537789e565a4427351958037bcdc35
Erin Hamrick <erin.hamrick76@gmail.com>
2023-09-06 11:04:19 -0500

    wrote inquirer prompts and started testing suite and shapes module

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const SVG = require('./lib/svg');
+
+async function generateLogo(){
+    inquirer.prompt([
+        {
+          type: 'input',
+          name: 'text',
+          message: 'Enter the text you would like displayed (3 characters or less):',
+          validate: input => input.length <= 3,
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'What color would you like your text to be? Enter color keyword or hexadecimal code:'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: "Please choose a shape for your logo:",
+            choices: 
+            [
+                'circle',
+                'triangle',
+                'square'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'bgColor',
+            message: 'What color would you like the backgroud to be? Enter color keyword or hexadecimal code'
+        }  
+    ]).then
+}
